f0c6ab224240e2dca69a2d566c129a3cc3ceac3a
DanDuarte24 <162132350+DanDuarte24@users.noreply.github.com>
2024-09-23 20:28:09 -0300

    Atualizando Models

---
+++ projeto-poo-alyce-danilo-henrique/src/main/java/com/example/ProjetoPOO/models/Usuario.java
@@ -0,0 +1,52 @@
+package mp.projetopoo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String login;
+    private String senha;
+
+    public Usuario() {}
+
+    public Usuario(String nome, String login, String senha) {
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha; }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
---
+++ projeto-poo-alyce-danilo-henrique/src/main/java/com/example/ProjetoPOO/models/Venda.java
@@ -0,0 +1,50 @@
+package mp.projetopoo.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Venda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Cliente cliente;
+
+    @ManyToOne
+    private Produto produto;
+
+    private String formaPagamento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+}
